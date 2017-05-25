1372b0409a4b3dfbd630cdbb13cd3e0cd2d4c3ef
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2017-05-25 20:58:35 +0530

    Update companies/:id/edit

--- routes/api/companies.js
+++ routes/api/companies.js
@@ -3,17 +3,16 @@ const models = require('./../../db/models').models;
 const password = require('./../../utils/password');
 
 router.post('/add', function (req, res) {
-    //these details would be validated by the frontend that none of the field is empty
-    // if (req.body.name === "" || req.body.email === "" || req.body.password === "") {
-    //     res.send("Insufficient Details");
-    // }
+    if (req.body.name === "" || req.body.email === "" || req.body.password === "") {
+        res.send("Insufficient Details");
+    }
     password.pass2hash(req.body.password).then(function (hash) {
         models.Company.create({
             name: req.body.name,
             email: req.body.email,
             password: hash
         }).then(function (company) {
-            res.redirect('/api/companies/' + company.id);
+            res.send(company.get());
         })
     })
 });
@@ -23,7 +22,7 @@ router.get('/:id', function (req, res) {
     models.Company.findOne({
         where: {id: companyId}
     }).then(function (company) {
-        res.send(company.get()); //Ask if user or user.dataValues
+        res.send(company.get());
     }).catch(function (err) {
         console.log(err);
         res.send('Unknown Company or unauthorized request');
@@ -32,30 +31,6 @@ router.get('/:id', function (req, res) {
 
 router.post('/:id/edit', function (req, res) {
     let companyId = parseInt(req.params.id);
-    //     let name = req.body.name === "" ? company.name : req.body.name,
-    //         email = req.body.email === "" ? company.email : req.body.email,
-    //         website = req.body.website === "" ? company.website : req.body.website,
-    //         locations = req.body.locations === "" ? company.locations : req.body.locations,
-    //         skills = req.body.skills === "" ? company.skills : req.body.skills,
-    //         repName = req.body.repName === "" ? company.repName : req.body.repName,
-    //         repNumber = req.body.repNumber === "" ? company.repNumber : req.body.repNumber;
-    //
-    //     models.Company.update({
-    //         name: req.body.name ,
-    //         email: email,
-    //         website: website,
-    //         locations: locations,
-    //         skills: skills,
-    //         repName: repName,
-    //         repNumber: repNumber
-    //     }, {
-    //         where: {id: companyId}
-    //     }).then(function (company) {
-    //         res.redirect('/api/companies/' + company.id);
-    //     }).catch(function (err) {
-    //     res.send('Unknown Company or unauthorized request');
-    // });
-
     email = req.body.email;
     website = req.body.website;
     locations = req.body.locations;
@@ -71,23 +46,23 @@ router.post('/:id/edit', function (req, res) {
         repName: repName,
         repNumber: repNumber
     }, {
-        where: {id: req.params.id},
+        where: {id: companyId},
         returning: true
     }).then(function (rows) {
         const company = rows[1][0].get();
         res.send(company);
-    }).catch(function (error) {
-        console.log(error);
+    }).catch(function (err) {
+        console.log(err);
     });
 
 });
 
-router.post('/:id/myJobs', function (req, res) {
+router.post('/:id/jobs', function (req, res) {
     let companyId = parseInt(req.params.id);
     models.Job.findAll({
         where: {companyId: companyId}
     }).then(function (jobs) {
-        res.send(jobs);
+        res.send(jobs.get());
     }).catch(function (err) {
         console.log(err);
         res.send("Unknown company or unauthorized access");
